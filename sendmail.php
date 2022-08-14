1fb6ec6352297143f9956fb9fde718ddd02f8398
Dudikoff Rakotomanga <drakotomanga@bocasay.com>
2022-08-14 17:33:33 +0300

    sen mail

--- sendmail.php
+++ sendmail.php
@@ -4,21 +4,16 @@ use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-require 'src/Exception.php';
-require 'src/PHPMailer.php';
-require 'src/SMTP.php';
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
-
-if(isset($_POST)) {
-    // Récupérer les données transmis depuis le formulaire
-    $prenom = $_POST['prenom'];
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-    $objet = $_POST['objet'];
-    $message = $_POST['message'];
-
+function phpmailersender(
+    $prenom,$nom,$email,$objet,$message
+)
+{
     // Créer une nouvelle instance de PHPMailer
-    $mail = new PHPMailer();
+    $mail = new PHPMailer(true);
 
     // Utiliser SMTP
     $mail->isSMTP();
@@ -29,6 +24,9 @@ if(isset($_POST)) {
     $mail->SMTPAuth = true;
     $mail->Username = 'tiana.vola39@gmail.com';
     $mail->Password = 'Ri@147852';
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;    
 
     // Adresse expéditeur
     $mail->setFrom('tiana.vola39@gmail.com', 'Gasy');
@@ -48,12 +46,48 @@ if(isset($_POST)) {
         'Message : '.$message.'<br>'
     );
 
+    return $mail->send();
+}
+
+function simpleMailSender(
+    $email,$objet,$message
+)
+{
+    return mail(
+         $email,
+         $objet,
+         $message,
+        [],
+        ""
+    );
+}
+
+if(isset($_POST)) {
+    // Récupérer les données transmis depuis le formulaire
+
+    //pour Logger les variables sur le serveur
+    //var_dump($_POST);
+
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $objet = $_POST['objet'];
+    $message = $_POST['message'];
+
+    $mailMessage = 'Prénom : '.$prenom.'<br>'.
+    'Nom : '.$nom.'<br>'.
+    'E-mail : '.$email.'<br>'.
+    'Objet : '.$objet.'<br>'.
+    'Message : '.$message.'<br>';
+
     // Envoi du message
-    if (!$mail->send()) {
-        echo 'Erreur : ' . $mail->ErrorInfo;
+    if (!simpleMailSender(
+        $email, $objet , $mailMessage
+    )) {
+        echo json_encode(array('message' =>  "failed"));
     } else {
-        echo 'Le mail a été envoyé !';
+        echo json_encode(array('message' =>  "success"));
     }
 } else {
-    echo 'Vous ne pouvez utiliser ce script directement !!!';
+    echo json_encode(array('message' =>  "cannot use this script!!"));
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8e7209f0cfbb8f459e5089ddcbcb9df16bef4b8e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e7209f0cfbb8f459e5089ddcbcb9df16bef4b8e',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.3',
+      'version' => '6.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9400f305a898f194caff5521f64e5dfa926626f3',
+    ),
+  ),
+);
